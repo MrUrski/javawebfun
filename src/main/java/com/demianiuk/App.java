b08572f17f91130421592a4632144293f7ff784e
SG0224607 <mateusz.demianiuk@sabre.com>
2016-04-07 09:36:25 +0200

    Basic maven structure.

---
+++ src/main/java/com/demianiuk/App.java
@@ -0,0 +1,13 @@
+package com.demianiuk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
